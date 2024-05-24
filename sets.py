c5eb809236a37579bfa27247b3443ceddc732672
Bautista-Maurizio <83047373+Bautista-Maurizio@users.noreply.github.com>
2024-05-24 11:52:51 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -2,30 +2,22 @@ from sets_categories_data import (ALCOHOLS)
 
 
 def clean_ingredients(dish_name, dish_ingredients):
-    """Remove duplicates from `dish_ingredients`.
+    clean_list = set(dish_ingredients)
+    return dish_name, clean_list
+    #set1= set()
+    #for item in dish_ingredients:
+        #set1.add(item)
+        #return dish_name, set1
 
-    :param dish_name: str - containing the dish name.
-    :param dish_ingredients: list - dish ingredients.
-    :return: tuple - containing (dish_name, ingredient set).
-
-    This function should return a `tuple` with the name of the dish as the first item,
-    followed by the de-duped `set` of ingredients as the second item.
-    """
-
-    return ()
 
 
 def check_drinks(drink_name, drink_ingredients):
-    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
-
-    :param drink_name: str - name of the drink.
-    :param drink_ingredients: list - ingredients in the drink.
-    :return: str - drink_name appended with "Mocktail" or "Cocktail".
-
-    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
-    name followed by "Cocktail" (includes alcohol).
-
-    """
-
-    return ""
+    clean_list = set(drink_ingredients)
+    from sets_categories_data import ALCOHOLS
+    if clean_list.intersection(ALCOHOLS):
+        drink_name = (f"{drink_name} Cocktail")
+        return drink_name
+    else:
+        drink_name = (f"{drink_name} Mocktail")
+        return drink_name
 
